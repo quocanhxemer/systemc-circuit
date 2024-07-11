b6d5154afc556fd6bd025f1bb2ff4d266edff40c
Tuankhang1411 <khangmastermath@gmail.com>
2024-07-11 12:25:04 +0200

    add main.c file

---
+++ src/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stddef.h>
+
+#include "Result.hpp"
+
+#include "args_parser.h"
+#include "csv_reader.h"
+
+extern struct Result run_simulation(
+        int cycles,
+        unsigned l1CacheLines,
+        unsigned l2CacheLines,
+        unsigned cacheLineSize,
+        unsigned l1CacheLatency,
+        unsigned l2CacheLatency,
+        unsigned memoryLatency,
+        size_t numRequests,
+        struct Request *requests,
+        const char *tracefile
+);
+
+int main(int argc, char *argv[]) {
+    struct arguments* args = parse_args(argc, argv);
+
+
+    printf("Einstellungen:\n");
+    printf("  cycles: %d\n", args->cycles);
+    printf("  cacheline_size: %d\n", args->cacheLineSize);
+    printf("  l1_lines: %d\n", args->l1CacheLines);
+    printf("  l2_lines: %d\n", args->l2CacheLines);
+    printf("  l1_latency: %d\n", args->l1CacheLatency);
+    printf("  l2_latency: %d\n", args->l2CacheLatency);
+    printf("  memory_latency: %d\n", args->memoryLatency);
+    if (args->tracefile != NULL) {
+        printf("  tracefile: %s\n", args->tracefile);
+    }
+    printf("  inputfile: %s\n", args->input_file);
+
+    struct csv_file_data data = csv_parse_file(args->input_file);
+
+    struct Result result = run_simulation(
+        args->cycles,
+        args->l1CacheLines,
+        args->l2CacheLines,
+        args->cacheLineSize,
+        args->l1CacheLatency,
+        args->l2CacheLatency,
+        args->memoryLatency,
+        data.lines,
+        data.data,
+        args->tracefile
+    );
+    free(args);
+    return 0;
+}
