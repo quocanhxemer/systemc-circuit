a6234c643f1d4af4116a67e23cf009b990785f66
Tuankhang1411 <khangmastermath@gmail.com>
2024-07-17 20:24:24 +0200

    add some tests for performance

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ extern struct Result run_simulation(
 
 
 int main(int argc, char *argv[]) {
-    struct arguments* args = parse_args(argc, argv);
+    struct arguments *args = parse_args(argc, argv);
     printf("Einstellungen:\n");
     printf("  cycles: %d\n", args->cycles);
     printf("  cacheline_size: %d\n", args->cacheLineSize);
@@ -31,30 +31,33 @@ int main(int argc, char *argv[]) {
     printf("  l2_latency: %d\n", args->l2CacheLatency);
     printf("  memory_latency: %d\n", args->memoryLatency);
     if (args->tracefile != NULL) {
-        printf("  tracefile: %s\n", args->tracefile);
+        printf("  tracefile: %s.vcd\n", args->tracefile);
     }
     printf("  inputfile: %s\n", args->input_file);
 
     struct csv_file_data data = csv_parse_file(args->input_file);
-
     struct Result result = run_simulation(
-        args->cycles,
-        args->l1CacheLines,
-        args->l2CacheLines,
-        args->cacheLineSize,
-        args->l1CacheLatency,
-        args->l2CacheLatency,
-        args->memoryLatency,
-        data.lines,
-        data.data,
-        args->tracefile
+            args->cycles,
+            args->l1CacheLines,
+            args->l2CacheLines,
+            args->cacheLineSize,
+            args->l1CacheLatency,
+            args->l2CacheLatency,
+            args->memoryLatency,
+            data.lines,
+            data.data,
+            args->tracefile
     );
-
-    printf("Cycles: %zu\n", result.cycles);
+    printf("Total Cycles: %zu\n", result.cycles);
     printf("Cache Hits: %zu\n", result.hits);
     printf("Cache Miss: %zu\n", result.misses);
-    printf("Primitive Gate Count: %zu\n", result.primitiveGateCount);
 
+    double hit_rate = (double) result.hits / (double) (result.hits+ result.misses);
+    printf("Hit rates: %.2f\n", hit_rate);
+    printf("Miss rates: %.2f\n", 1.0 - hit_rate);
+    double avg= (double)result.cycles/ (double)data.lines;
+    printf("Average pro Memory Access: %.2f Cycles per Instruction \n", avg);
+    printf("Primitive Gate Count: %zu\n", result.primitiveGateCount);
     free(args);
     return 0;
 }
